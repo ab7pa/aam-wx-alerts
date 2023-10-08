dce9feb0ce8bb32ddb05d447f274399b09e2625e
ab7pa <ab7pa.radio@gmail.com>
2023-10-08 16:41:49 -0700

    changed file location and added README

--- src/data/etc/cron.hourly/get-wx-alerts.lua
+++ src/data/etc/cron.hourly/get-wx-alerts.lua
@@ -1,18 +1,18 @@
 #!/usr/bin/lua
 -- Program:  get-wx-alerts.lua
--- Purpose:  To retrieve any current NWS weather alerts and update the AAM wx.txt file
--- wx-alert-config.lua contains alert file path and weather zone to query
---   wx_alert_file   = "/tmp/aam/wx.txt"
---   wx_zone         = "AZZ548"
-
-package.path = package.path .. ";/tmp/wx-alert/?.lua"
+-- Purpose:  To retrieve any current NWS weather alerts for a zone and update /www/wx.txt
 
 require("luci.jsonc")
 require("aredn.utils")
 require("nixio")
-require("wx-alert-config")
 
--- Initialize the wx alert file
+-- Initialize variables and wx_alert_file
+wx_alert_file   = "/www/aam/wx.txt"
+--wx_zone         = "AZZ548"
+wx_zone         = "AKZ332"
+connect_timeout = 5
+speed_time      = 10
+speed_limit     = 1000
 os.execute("cat /dev/null > " .. wx_alert_file)
 
 -- Query weather.gov API for zone alerts
@@ -21,19 +21,10 @@ wx = capture("/usr/bin/curl --silent --retry 3 --connect-timeout " .. connect_ti
 if wx then
   local wxalerts = luci.jsonc.parse(wx)
   if wxalerts.features[1] then   -- Has WX alert info
---    print(wxalerts.title)
---    print(wxalerts.features[1].properties.headline)
     local f = io.open(wx_alert_file, "w")
     if f then
       f:write(wxalerts.title .. "<br>" .. wxalerts.features[1].properties.headline )
       f:close()
     end
---  else   -- No WX alert info
---    print(wxalerts.title .. " - No current weather alerts")
   end
-
---else   -- Was not able to contact the NWS api
---  print("Unable to contact NWS api")
-
 end
-
--- src/data/tmp/wx-alerts/wx-alert-config.lua
+++
@@ -1,6 +0,0 @@
-wx_alert_file   = "/www/aam/wx.txt"
---wx_zone         = "AZZ548"
-wx_zone         = "AKZ332"
-connect_timeout = 5
-speed_time      = 10
-speed_limit     = 1000
